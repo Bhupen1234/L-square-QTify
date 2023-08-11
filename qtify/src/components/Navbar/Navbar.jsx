5867e6b05fdba72242a7c1fe5ab82d0854f4aad0
Bhupendra <bhupendranasare@gmail.com>
2023-08-11 14:58:50 +0530

    C4: Search Bar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Button from '../Button/Button'
 import Logo from '../Logo/Logo'
 import styles from './Navbar.module.css'
+import SearchBar from '../SearchBar/SearchBar'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
+    
       <Logo/>
+      <SearchBar placeholder="Search a album of your choice"/>
       <Button children="Give Feedback"/>
     </nav>
   )
---
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg'
+import styles from "./Searchbar.module.css"
+const SearchBar = ({placeholder}) => {
+  return (
+    <>
+    <form className={styles.wrapper}>
+        <input required className={styles.search} placeholder={placeholder} />
+        <button type='submit' className={styles.searchButton}>
+            <SearchIcon/>
+        </button>
+    </form>
+    </>
+  )
+}
+
+export default SearchBar
